8ffa725f9fc156a9c8aa5bdbc9559e0566bc2d13
chrompay <aghoghohappy8@gmail.com>
2025-07-23 09:34:05 +0100

    This is a tutorial on variable for flutter

---
+++ Class1.dart
@@ -0,0 +1,44 @@
+void main() {
+  var address = 'Lokogoma';
+  var numberInClass = 5;
+
+  print(numberInClass);
+  print(address);
+
+  var myguy;
+  myguy = 'Happy';
+
+  String Class = 'God has blessed $myguy';
+  print(Class);
+
+  String firstName = 'Happy';
+  String lastName = 'Obrirhe';
+  String fullName = firstName + ' ' + lastName;
+  print(fullName);
+
+  int noOfStudents = 6;
+  print(noOfStudents);
+
+  double height = 24.4;
+  print(height);
+
+  num time = 4.27;
+  print(time);
+
+  bool amHere = false;
+  print(amHere);
+
+  String name = 'Grace';
+  String location = 'Abuja';
+  String NameLocation = name + ' ' + location;
+  print(NameLocation);
+
+  String message =
+      'this' +
+      ''' is the day
+which the Lord has made,
+let us rejoice
+and be glad in it. $name''';
+
+  print(message);
+}
